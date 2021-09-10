aafa92dbca9313b3389e32c7876250bd9602e2b6
BenoitRobin <benoitrobin@me.com>
2021-09-10 19:42:30 +0200

    set Media Queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,21 +46,21 @@ skillsHeader.forEach((e) => {
 });
 
 /*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll("[data-target]"),
-    tabContents = document.querySelectorAll("[data-content]");
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach((tab) => {
-    tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.target);
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
 
-    tabContents.forEach((tabContent) => {
-        tabContent.classList.remove("qualification__active");
-    });
-    target.classList.add("qualification__active");
+    tabContents.forEach(tabContent => {
+        tabContent.classList.remove("qualification__active")
+    })
+    target.classList.add("qualification__active")
 
-    tab.forEach((tab) => {
-        tab.classList.remove("qualification__active");
-    });
+    tabs.forEach(tab => {
+        tab.classList.remove('qualification__active')
+    })
     tab.classList.add("qualification__active");
     });
 });
